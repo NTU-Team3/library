ce3f2d0d44638744c80011ffa31ef6b30fdab02e
swaichee <98152745+swaichee@users.noreply.github.com>
2022-05-10 08:23:05 +0800

    User table schema

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const { sequelize } = require("../config/db");
+const { DataTypes, Model } = require("sequelize");
+
+class User extends Model {}
+
+User.init(
+  // column config
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING(32),
+      allowNull: false,
+      field: "email",
+    },
+    password: {
+      type: DataTypes.STRING(13),
+      allowNull: false,
+      field: "password",
+    },
+    name: {
+      type: DataTypes.STRING(150),
+      allowNull: true,
+      field: "name",
+    },
+    noOfBook: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      field: "no_of_book",
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      field: "created_at",
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      field: "updated_at",
+    },
+  },
+  // Model config
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "Users",
+  }
+);
+
+module.exports = User;
