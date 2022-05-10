790546fe1889d83825df5a672c0a1e0cc56baadf
Saiful <saifu7bahri@gmail.com>
2022-05-10 23:05:38 +0800

    Add reviews.js

---
+++ models/review.js
@@ -0,0 +1,43 @@
+const { sequelize } = require("../config/db");
+const { DataTypes, Model } = require("sequelize");
+
+class Review extends Model {}
+
+Review.init(
+  // column config
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    ratings: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: "rating",
+    },
+    comments: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: "your comments",
+    },
+    userID: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      field: "user",
+    },
+    book: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      field: "book title",
+    },
+  },
+  // Model config
+  {
+    sequelize,
+    modelName: "Review",
+    tableName: "Reviews",
+  }
+);
+
+module.exports = Review;
