228a201502d3711bf6df8f12a384a36046927f64
swaichee <98152745+swaichee@users.noreply.github.com>
2022-05-10 08:24:26 +0800

    Admin Setup and Reset DB

---
+++ services/admin.service.js
@@ -0,0 +1,129 @@
+const { sequelize } = require("../config/db");
+
+const Book = require("../models/book");
+const User = require("../models/user");
+
+module.exports = {
+  //
+  setup: async () => {
+    const result = {
+      status: null,
+      message: null,
+      data: null,
+    };
+
+    /* create table Book */
+    await Book.sync({ alter: true });
+
+    /* insert bulk records into Book table */
+    const newBooks = await Book.bulkCreate([
+      {
+        isbn: "9780141439600",
+        title: "A Tale of Two Cities",
+        author: "Charles Dickens",
+        qty: 2,
+        availQty: 2,
+      },
+      {
+        isbn: "9788845292613",
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        qty: 3,
+        availQty: 3,
+      },
+      {
+        isbn: "9781932429237",
+        title: "Think and Grow Rich",
+        author: "Napoleon Hill",
+        qty: 1,
+        availQty: 1,
+      },
+      {
+        isbn: "9780830878024",
+        title: "The Greco-Roman World of the New Testament Era",
+        author: "James S. Jeffers",
+        qty: 1,
+        availQty: 1,
+      },
+    ]);
+
+    console.log("Creating User table");
+    /* Create User table */
+    await User.sync({ alter: true });
+
+    console.log("Bulk insert users");
+    /* Bulk insert records into User table */
+    const newUsers = await User.bulkCreate([
+      {
+        email: "help@gmail.com",
+        password: "LittleLamp",
+        name: "Charles Dickens",
+        noOfBook: 0,
+      },
+      {
+        email: "craxy@hotmail.com",
+        password: "Max%imum1",
+        name: "Code Hunter",
+        noOfBook: 0,
+      },
+      {
+        email: "in@gmail.com",
+        password: "@ttle00Lamp",
+        name: "Bookworms",
+        noOfBook: 0,
+      },
+      {
+        email: "speed@gmail.com",
+        password: "@ct0ber",
+        name: "Secret User",
+        noOfBook: 0,
+      },
+    ]);
+    // error handling
+    if (!newBooks) {
+      result.status = 404;
+      result.message = `Book list is empty`;
+      return result;
+    } else {
+      result.status = 200;
+      result.message = "List of books:";
+      result.data = newBooks;
+      return result;
+    }
+
+    // error handling
+    if (!newUsers) {
+      result.status = 404;
+      result.message = `User list is empty`;
+      return result;
+    } else {
+      result.status = 200;
+      result.message = "List of Users:";
+      result.data = newUsers;
+      return result;
+    }
+  },
+
+  reset: async () => {
+    const result = {
+      status: null,
+      message: null,
+      data: null,
+    };
+    // drop all tables in the database
+    const isSuccess = sequelize.drop();
+
+    // error handling
+    if (!isSuccess) {
+      result.status = 404;
+      result.message = `Failed to reset database`;
+      return result;
+    } else {
+      result.status = 200;
+      result.message = "Database has been reset!";
+      result.data = null;
+      return result;
+    }
+    //const users = await User.destroy();
+  },
+};
---
+++ services/public.service.js
@@ -0,0 +1,40 @@
+//const { sequelize } = require("../config/db");
+
+const Book = require("../models/book");
+
+module.exports = {
+  // Return all the books.
+  book: async () => {
+    const result = {
+      status: null,
+      message: null,
+      data: null,
+    };
+
+    //const isExist = Book.findAll();
+
+    console.log("Value of the Book", Book);
+    if (!Book) {
+      result.status = 404;
+      result.message = `The database has not been setup. Run setup to create all the tables.`;
+      return result;
+    }
+
+    const books = await Book.findAll({
+      order: [["title", "ASC"]],
+    });
+
+    // error handling
+    if (!books) {
+      result.status = 404;
+      result.message = `The librart is empty`;
+      return result;
+    }
+
+    result.status = 200;
+    result.message = "List of books in the library:";
+    result.data = books;
+
+    return result;
+  },
+};
