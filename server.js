a5a1d6d7f757aaab4070fe0bb3f29fc424d07c85
Royston Lau <roystonlau@gmail.com>
2022-05-11 12:09:21 +0800

    test

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const router = require("./routes/");
 const express = require("express");
 const app = express();
-const path = require("path");
-app.use(express.json()); // Enable express to parse JSON as request body.
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs");
-app.set("views", "views");
-
 const PORT = process.env.PORT || 3000;
+app.get("/", routes.index);
 
-router.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}...`);
+db.sequelize.sync().then(function () {
+  http.createServer(app).listen(app.get("port"), function () {
+    console.log("Express server listening on port " + app.get("port"));
+  });
 });
