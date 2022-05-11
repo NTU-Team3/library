f4c2d06d99409eff8494304668b17aed46f69e99
Royston Lau <roystonlau@gmail.com>
2022-05-11 14:18:18 +0800

    clean

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const router = require("./routes/");
 const express = require("express");
 const app = express();
 
-app.get("/", routes.index);
-
 const PORT = process.env.PORT || 3000;
 
 router.listen(PORT, () => {
