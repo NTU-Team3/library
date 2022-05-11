67f931849ad12dfd766f4d75bab62a2b109a4a97
Kwek Ming Sheng <kmings93@gmail.com>
2022-05-11 11:39:10 +0800

    added heroku

--- config/db.js
+++ config/db.js
@@ -1,8 +1,18 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("library_db", "postgres", "", {
-  host: "localhost",
+// const sequelize = new Sequelize("library_db", "postgres", "", {
+//   host: "localhost",
+//   dialect: "postgres",
+// });
+
+const sequelize = new Sequelize("postgres://fnbethehxrgjwi:e806c0ccec517359ddd9f5b446bbe229b4bd0f8a554b27fd17fa2271778a9588@ec2-54-172-175-251.compute-1.amazonaws.com:5432/dahngglaei1616", {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 const testConn = async () => {
