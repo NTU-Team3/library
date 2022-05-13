e99d469aa7958780ac10f2fef0deeffe5e1e9599
roystonlau <90206226+roystonlau@users.noreply.github.com>
2022-05-13 09:42:22 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  "postgres://suvcqyykxtoool:6d1e81c21301965163175c411e79bcdc7a776200acc3fb82c7ca66a51e0aa0e1@ec2-54-172-175-251.compute-1.amazonaws.com:5432/ddlfmjblfrar1e",
+  "postgres://uhqdsudkciqmvs:2413fa624382501afa1b87a73ec7f1f9326d71b3405b2de47af91a88486ab0f0@ec2-34-231-177-125.compute-1.amazonaws.com:5432/d7tq6hhv1akuff",
   {
     dialect: "postgres",
     dialectOptions: {
