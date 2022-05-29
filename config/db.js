5c70545a5fec739f0807ffe5291be5ea3499d3f8
trainingresult6361 <96628430+trainingresult6361@users.noreply.github.com>
2022-05-29 16:40:05 +0800

    Exclude sensitive info that resides in local .env file

--- config/db.js
+++ config/db.js
@@ -1,17 +1,16 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
+const db = process.env.DATABASE_URL || "http://localhost:3000/";
 
-const sequelize = new Sequelize(
-  "postgres://uhqdsudkciqmvs:2413fa624382501afa1b87a73ec7f1f9326d71b3405b2de47af91a88486ab0f0@ec2-34-231-177-125.compute-1.amazonaws.com:5432/d7tq6hhv1akuff",
-  {
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+const sequelize = new Sequelize(db, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
 
 const testConn = async () => {
   try {
